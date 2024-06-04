6d996b2437c31c533a78e8dd6b5a1672e32ad619
Sulaiman Hashim <93908604+sulaimanmhashim@users.noreply.github.com>
2024-06-04 13:38:08 +0800

    UI_basic
    created main menu and pause button

--- assets/Scripts/PlayerControl.gd
+++ assets/Scripts/PlayerControl.gd
@@ -7,15 +7,24 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+# Get the animatedsprite2d child node to control animation
+# use animatedsprite2d method like play() and stop() to control animation
+var playerAnim
+
+func _ready():
+	playerAnim = $PlayerAnim
+	playerAnim.play("default")
 
 func _physics_process(delta):
+	if(Input.is_anything_pressed()==false):
+		playerAnim.play("default")
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -23,8 +32,20 @@ func _physics_process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 	
 	if direction:
+		if is_on_floor():
+			playerAnim.play("run")
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if direction>0:
+		playerAnim.flip_h=false
+	elif direction<0:
+		playerAnim.flip_h=true
+	
+	if velocity.y>0:
+		playerAnim.play("fall")
+	elif velocity.y<0:
+		playerAnim.play("jump")
 
 	move_and_slide()
